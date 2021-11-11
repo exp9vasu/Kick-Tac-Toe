9ab4d0b184434c75ee4a821abb46f09b2d63e5c5
Vasu Pandey <65676114+exp9vasu@users.noreply.github.com>
2021-11-12 01:30:26 +0530

    first commit

---
+++ Assets/Scripts/DragAndShoot.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndShoot : MonoBehaviour
+{
+    private Vector3 mousePressDownPos;
+    private Vector3 mouseReleasePos;
+
+    private Rigidbody rb;
+
+    private bool isShoot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnMouseDown()
+    {
+        mousePressDownPos = Input.mousePosition;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 forceInit = (Input.mousePosition - mousePressDownPos);
+        Vector3 forceV = (new Vector3(forceInit.x, forceInit.y, z:forceInit.y))*forceMultiplier;
+
+        if (!isShoot)
+            //    Trajectory.Instance.UpdateTrajectory( forceVector: forceV, rb, startingPoint: transform.position);
+            Trajectory.Instance.UpdateTrajectory(forceVector: forceV, rb,  Vector3.zero);
+    }
+
+    private void OnMouseUp()
+    {
+        Trajectory.Instance.HideLine();
+        mouseReleasePos = Input.mousePosition;
+        Shoot(Force:mousePressDownPos - mouseReleasePos);
+    }
+
+    private float forceMultiplier = 5;
+
+    void Shoot(Vector3 Force)
+    {
+        if (isShoot)
+            return;
+
+        rb.AddForce(new Vector3(Force.x, Force.y, z: Force.y)* forceMultiplier);
+        isShoot = true;
+    }
+}
---
+++ Assets/Scripts/Trajectory.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trajectory : MonoBehaviour
+{
+    [SerializeField]
+    private LineRenderer _linerenderer;
+    [SerializeField]
+    [Range(3, 30)]
+    private int _lineSegmentCount = 20;
+
+    private List<Vector3> _linePoints = new List<Vector3>();
+
+    public static Trajectory Instance;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    private void Start()
+    {
+       
+    }
+
+    public void UpdateTrajectory(Vector3 forceVector, Rigidbody rigidbody , Vector3 startingPoint)
+    {
+        Vector3 velocity = (forceVector / rigidbody.mass) * Time.fixedDeltaTime;
+
+        float FlightDuration = (2 * velocity.y) / Physics.gravity.y;
+
+        float stepTime = FlightDuration / _lineSegmentCount;
+
+        _linePoints.Clear();
+
+        for(int i = 0; i< _lineSegmentCount; i++)
+        {
+            float stepTimePassed = stepTime * i;
+
+            Vector3 MovementVector = new Vector3(
+                x: velocity.x * stepTimePassed,
+                y: velocity.y * stepTimePassed - 0.5f * Physics.gravity.y * stepTimePassed * stepTimePassed,
+                z: velocity.z * stepTimePassed
+                ) ;
+            if (i == 0)
+            {
+                _linePoints.Add(item: startingPoint);
+            }
+            else
+            {
+                _linePoints.Add(item: -MovementVector + startingPoint);
+            }
+    
+        }
+
+        
+        _linerenderer.positionCount = _linePoints.Count;
+        _linerenderer.SetPositions(_linePoints.ToArray());
+    }
+        public void HideLine()
+    {
+        _linerenderer.positionCount = 0;
+    } 
+
+}
